9c1a78e6ca60975ef45f2f87a083a94dc02e0743
ElNegro <105134795+ElNegroW@users.noreply.github.com>
2024-07-01 23:12:15 +0000

    Mundo guardado 2024-07-01

--- server.py
+++ server.py
@@ -9,7 +9,7 @@
 # Código          Lugar
 #-----------      ---------------------------
 # ap	          Asia / Pacífico (Singapore)
-# au		      Australia 1(Sydney)
+# au		      Australia (Sydney)
 # eu		      Europa (Frankfurt)
 # in		      India (Mumbai)
 # jp		      Japón (Tokyo)
